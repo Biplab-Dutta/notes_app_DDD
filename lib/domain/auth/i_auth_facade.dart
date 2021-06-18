525160b50a9c64a23c7e7ae11cc3b309d33bccec
Biplab-Dutta <biplabdutta860@gmail.com>
2021-06-18 12:51:33 +0545

    Created interface, auth_failure and value_objects regarding auth

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import 'package:notes_app/domain/auth/auth_failure.dart';
+import 'package:notes_app/domain/auth/value_objects.dart';
+
+// ! This class is similar to repository in the domain layer of CLEAN Architecture
+// ! Here we are only speciying the contracts.
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:notes_app/domain/core/failures.dart';
+import 'package:notes_app/domain/core/value_validators.dart';
+
+class EmailAddress extends Equatable {
+  final Either<ValueFailure<String>, String> value;
+  factory EmailAddress(String input) =>
+      EmailAddress._(validateEmailAddress(input));
+
+  const EmailAddress._(this.value);
+
+  bool isValid() => value.isRight();
+
+  @override
+  List<Object> get props => [value];
+}
+
+class Password extends Equatable {
+  final Either<ValueFailure<String>, String> value;
+  factory Password(String input) => Password._(validatePassword(input));
+
+  const Password._(this.value);
+
+  bool isValid() => value.isRight();
+
+  @override
+  List<Object> get props => [value];
+}
