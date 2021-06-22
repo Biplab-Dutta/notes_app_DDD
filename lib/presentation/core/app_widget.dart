53b103b44bcdf7c0cfc2e136ecd551796d432f3e
Biplab-Dutta <biplabdutta860@gmail.com>
2021-06-22 18:23:33 +0545

    Added sign in page

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/presentation/sign_in/sign_in_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.green,
+        primaryColor: Colors.green,
+        accentColor: Colors.blueAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+      home: const SignInPage(),
+    );
+  }
+}
